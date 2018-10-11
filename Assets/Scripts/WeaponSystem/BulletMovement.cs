4e45b5dedb4ca0974c0ea41e98d8ba54f51751c5
Mark DelFavero <markadelf@gmail.com>
2018-10-11 00:06:28 -0400

    Added Bullet Pool Support

--- Assets/Scripts/WeaponSystem/BulletMovement.cs
+++ Assets/Scripts/WeaponSystem/BulletMovement.cs
@@ -7,6 +7,7 @@ public class BulletMovement : MonoBehaviour {
     private Vector3 velocity;
     private int damage;
     private float life;
+    private BulletPool pool;
 
     private CharacterController controller;
 
@@ -18,12 +19,19 @@ public class BulletMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         life -= Time.deltaTime;
-        if(life <= 0)
-        {
-            Destroy(gameObject);
-        }
         controller.Move(velocity * Time.deltaTime);
-	}
+        if (life <= 0)
+        {
+            if (pool != null)
+            {
+                pool.Free(gameObject);
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+    }
 
     // Initializes it with direction, damage and lifespan
     public void SetUp(Vector3 velocity, int damage, float lifespan)
@@ -31,5 +39,10 @@ public class BulletMovement : MonoBehaviour {
         this.velocity = velocity;
         this.damage = damage;
         life = lifespan;
+    }
+
+    public void SetPool(BulletPool pool)
+    {
+        this.pool = pool;
     }
 }
--- Assets/Scripts/WeaponSystem/WeaponPickup.cs
+++ Assets/Scripts/WeaponSystem/WeaponPickup.cs
@@ -19,17 +19,23 @@ public class WeaponPickup : MonoBehaviour {
     private int currentAmmo;
     private float coolDown = 0;
     private bool held;
+    private BulletPool pool;
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         currentAmmo = MaxAmmo;
         coolDown = 0;
-        if(Muzzle == null)
+        if (Muzzle == null)
         {
             Muzzle = transform;
         }
-	}
+        if (BulletPool.PoolDirectory.ContainsKey(Bullet))
+        {
+            pool = BulletPool.PoolDirectory[Bullet];
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -52,7 +58,19 @@ public class WeaponPickup : MonoBehaviour {
             // Fire the whole volley
             for (int i = 0; i < Volley && HasAmmo(); i++)
             {
-                GameObject fired = Instantiate(Bullet);
+                GameObject fired = null;
+                if (pool != null)
+                {
+                    fired = pool.GetBullet();
+                }
+                else
+                {
+                    fired = Instantiate(Bullet);
+                }
+                if (fired == null)
+                {
+                    return false;
+                }
                 fired.transform.position = pos;
                 float angle = Random.Range(-Spread, Spread);
                 BulletMovement movement = fired.GetComponent<BulletMovement>();
