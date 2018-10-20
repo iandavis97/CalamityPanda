1efcdf79a1fc73a676ea836eb6e77dfcaca8a826
Mark DelFavero <markadelf@gmail.com>
2018-10-20 17:14:34 -0400

    Minor changes to health pickup.

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class PowerUp : MonoBehaviour {
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(TryUse(collision.gameObject))
         {
