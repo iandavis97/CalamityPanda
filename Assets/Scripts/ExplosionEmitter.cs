037d6392ed320e36d684d700b4200dc03ab8240a
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2018-10-21 12:16:32 -0400

    Explosions cause damage but are invisible

---
+++ Assets/Scripts/ExplosionEmitter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionEmitter : MonoBehaviour
+{
+    static GameObject explosion;
+	// Use this for initialization
+	void Start ()
+    {
+		if (explosion == null)
+        {
+            explosion = Resources.Load<GameObject>("Explosion");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public IEnumerator Explode()
+    {
+        GameObject explosionInstance = Instantiate(explosion, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(1);
+        Destroy(explosionInstance);
+    }
+}
