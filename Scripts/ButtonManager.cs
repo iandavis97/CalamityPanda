2055344c5162436872bfe778314cf4ac9d8ba3a2
iandavis97 <iandavis97@gmail.com>
2018-10-20 23:02:51 -0400

    Door and key scripts

---
+++ Scripts/ButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Ingame_UI");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void TitleScreenButton()
+    {
+        SceneManager.LoadScene("Title_Screen");
+    }
+
+    public void EndGameScreen()
+    {
+        SceneManager.LoadScene("End_Game_Screen");
+    }
+
+}
