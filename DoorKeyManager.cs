003640f739431d3e0142db15fa217f539a593c28
iandavis97 <iandavis97@gmail.com>
2018-10-29 18:17:48 -0400

    finished Door system
    Keys can be picked up pressing the E key

---
+++ DoorKeyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKeyManager : MonoBehaviour
+{
+    public List<GameObject> DoorList;//list of all locked doors in scene
+    public List<GameObject> KeyList;//list of all keys in scene
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        for (int i = 0; i < DoorList.Capacity; i++)//looping through doors
+        {
+            Key keyScript = KeyList[i].GetComponent<Key>();
+            //checking if any keys were grabbed
+            if (keyScript.KeyGrabbed==true)
+            {
+                //destroys corresponding door when correct key grabbed
+                Destroy(DoorList[i]);
+            }
+        }
+	}
+}
---
+++ Key.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public bool KeyGrabbed;//checks if player has got this key
+    public GameObject player;
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        KeyCollision(player);
+        if (KeyGrabbed == true)
+            this.GetComponent<SpriteRenderer>().enabled = false;
+	}
+    private void KeyCollision(GameObject other)
+    {
+        if (Input.GetKeyDown(KeyCode.E)
+            &&(this.GetComponent<Collider2D>().IsTouching(other.GetComponent<Collider2D>()) ==true))//picking up key
+        {
+            KeyGrabbed = true;
+        }
+        else
+            KeyGrabbed = false;
+    }
+}
