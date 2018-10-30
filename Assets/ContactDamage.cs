54d9e5a1a60959eda01f1fe1eb56f5907057ef2b
Mark DelFavero <markadelf@gmail.com>
2018-10-29 23:53:05 -0400

    Added damage to the spin dash above certain speeds

---
+++ Assets/ContactDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactDamage : MonoBehaviour {
+
+    public float MinSpeedForDamage = 10;
+    private Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+        rigid = transform.root.GetComponentInChildren<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (rigid.velocity.sqrMagnitude >= MinSpeedForDamage * MinSpeedForDamage)
+        {
+            Damagable damage = collision.GetComponent<Damagable>();
+            if(damage != null)
+            {
+                damage.TakeDamage(4);
+            }
+        }
+    }
+}
