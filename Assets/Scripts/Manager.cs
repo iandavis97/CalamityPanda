e81cc1c214501f980e0c674d134f74c3a5380f33
boringlawyer <cnk5777@rit.edu>
2018-10-30 00:24:42 -0400

    Added Final Scene and Level Manager

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour 
+{
+	Queue<BasicEnemy> inactiveEnemies;
+	[SerializeField]
+	float enemyActivationInterval;
+	// Use this for initialization
+	void Start () 
+	{
+		BasicEnemy[] enemies = GameObject.FindObjectsOfType<BasicEnemy>();
+		for	(int i = 0; i < enemies.Length; ++i)
+		{
+			enemies[i].gameObject.SetActive(false);
+		}
+		inactiveEnemies = new Queue<BasicEnemy>(enemies);
+		StartCoroutine(ActivateEnemies());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	IEnumerator ActivateEnemies()
+	{
+		yield return new WaitForSeconds(enemyActivationInterval);
+		inactiveEnemies.Dequeue().gameObject.SetActive(true);
+		StartCoroutine(ActivateEnemies());
+	}
+}
