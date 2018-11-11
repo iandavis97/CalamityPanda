5b1bc920e66a687fab0781cb36f65b5145bc01fb
mxo9722 <mxo9722@rit.edu>
2018-11-10 22:22:44 -0500

    first commit
    first push for branch

---
+++ Assets/Scripts/Pathfinding/Node.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+	Vector2 pos;
+
+	List<GameObject> neighbors=new List<GameObject>();
+
+	// Use this for initialization
+	void Start(){
+
+		pos = new Vector2 (gameObject.transform.position.x,gameObject.transform.position.y);
+
+	}
+
+	public void Connect(Node n, bool mutual=false)
+	{
+
+		if (mutual) {
+			neighbors.Add (n.gameObject);
+			return;
+		}
+
+		if (n.DistanceTo(pos)<=1) 
+		{
+			neighbors.Add (n.gameObject);
+			n.Connect (this,true);
+		}
+
+
+	}
+
+	public Vector2 Pos {
+		get {
+			return pos;
+		}
+	}
+
+	bool LineObstructed(Node n, NodeList list)
+	{
+		
+		if (Physics2D.Linecast (pos, n.Pos)) 
+		{
+			return true;
+		}
+
+		return false;
+
+	}
+
+	bool LinePermObstructed(Node n, NodeList list)
+	{
+
+		//Collider2D
+
+		if (Physics2D.Linecast (pos, n.Pos)) 
+		{
+			return true;
+		}
+
+		return false;
+
+	}
+
+	float DistanceTo(Vector2 dPos)
+	{
+		return Vector2.Distance(dPos,pos);
+	}
+
+	void FixedUpdate(){
+
+		foreach (GameObject n in neighbors)
+			Debug.DrawLine(pos,n.GetComponent<Node>().Pos);
+
+	}
+}
---
+++ Assets/Scripts/Pathfinding/NodeList.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeList : MonoBehaviour {
+
+	public List<GameObject> content = new List<GameObject>();
+	List<GameObject> pObstructions = new List<GameObject> ();
+	List<GameObject> tObstructions = new List<GameObject> ();
+
+	public GameObject nodePrefab;
+
+	public int minX;
+	public int minY;
+	public int maxX;
+	public int maxY;
+
+	// Use this for initialization
+	void Start () {
+
+		pObstructions = new List<GameObject>(GameObject.FindGameObjectsWithTag ("pObstruction"));
+		tObstructions =  new List<GameObject>(GameObject.FindGameObjectsWithTag ("tObstruction"));
+		content = new List<GameObject> (GameObject.FindGameObjectsWithTag("node"));
+
+		for(int i=0;i<content.Count;i++){
+		
+			for (int j = 0; j < content.Count; j++) 
+			{
+				if (j == i)
+					continue;
+
+				content [i].GetComponent<Node> ().Connect (content [j].GetComponent<Node>());
+
+			}
+		
+		}
+
+	}
+
+	public List<GameObject> PObstructions {
+		get {
+			return pObstructions;
+		}
+	}
+
+	public List<GameObject> TObstructions {
+		get {
+			return tObstructions;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
